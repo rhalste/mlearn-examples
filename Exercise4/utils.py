5b796181c206e9b07f13ab3a47b0c68b77f84b24
rhalste <50845904+rhalste@users.noreply.github.com>
2021-08-24 12:12:31 -0400

    Add Timer to utils

--- Exercise4/utils.py
+++ Exercise4/utils.py
@@ -5,6 +5,33 @@ from matplotlib import pyplot
 sys.path.append('..')
 from submission import SubmissionBase
 
+import time
+from datetime import datetime
+
+class TimerError(Exception):
+    """A custom exception used to report errors in use of Timer class"""
+
+class Timer:
+    def __init__(self):
+        self._start_time = None
+
+    def start(self):
+        """Start a new timer"""
+        print(datetime.now().strftime("Start time: %m/%d/%Y, %H:%M:%S"))
+        if self._start_time is not None:
+            raise TimerError(f"Timer is running. Use .stop() to stop it")   
+
+        self._start_time = time.perf_counter()
+
+    def stop(self):
+        """Stop the timer, and report the elapsed time"""
+        if self._start_time is None:
+            raise TimerError(f"Timer is not running. Use .start() to start it")
+
+        elapsed_time = (time.perf_counter() - self._start_time)/60.0
+        self._start_time = None
+        print(f"Elapsed time: {elapsed_time:0.2f} minutes")
+
 
 def displayDataInv(X, example_width=None, figsize=(10, 10)):
     """
